7e54300b3881327cfd1ba1acaaf2855c84dcfce4
Julia Qiu <juliaqiuxy@gmail.com>
2018-03-21 17:29:24 -0700

    add sleet weather icon

--- src/components/ResortInfoCard/ResortInfoCardHeader.js
+++ src/components/ResortInfoCard/ResortInfoCardHeader.js
@@ -14,6 +14,7 @@ import CloudyIcon from './WeatherIcons/CloudyIcon';
 import RainIcon from './WeatherIcons/RainIcon';
 import SnowIcon from './WeatherIcons/SnowIcon';
 import ThunderStormIcon from './WeatherIcons/ThunderStormIcon';
+import SleetIcon from './WeatherIcons/SleetIcon';
 
 import { resortLogos } from '../SlideBar/ResortNavCard';
 
@@ -25,7 +26,9 @@ const weatherIcons = {
   rain: RainIcon,
   snow: SnowIcon,
   thunderstorm: ThunderStormIcon,
+  sleet: SleetIcon,
 };
+
 const DEVICE_WIDTH = Dimensions.get('window').width;
 
 const styles = StyleSheet.create({
